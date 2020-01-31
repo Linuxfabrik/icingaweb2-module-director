8f2c46ed3fdfbcc505474d79ee422fb15dbeb090
Navid Sassan <navid.sassan@linuxfabrik.ch>
2020-01-31 14:17:29 +0100

    increments director_schema version

--- schema/mysql-migrations/upgrade_169.sql
+++ schema/mysql-migrations/upgrade_169.sql
@@ -4,4 +4,4 @@ ALTER TABLE icinga_command ADD COLUMN guid CHAR(36) DEFAULT NULL AFTER zone_id;
 
 INSERT INTO director_schema_migration
   (schema_version, migration_time)
-  VALUES (168, NOW());
+  VALUES (169, NOW());
