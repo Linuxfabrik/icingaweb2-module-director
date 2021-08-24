8a5b67e209632da68b5f3961b012b4ae32fe1b59
Navid Sassan <navid.sassan@linuxfabrik.ch>
2021-08-24 11:53:51 +0200

    use Ramsey to generate GUIDs

--- library/Director/Data/Db/DbObject.php
+++ library/Director/Data/Db/DbObject.php
@@ -11,6 +11,7 @@ use LogicException;
 use RuntimeException;
 use Zend_Db_Adapter_Abstract;
 use Zend_Db_Exception;
+use Ramsey\Uuid\Uuid;
 
 /**
  * Base class for ...
@@ -799,7 +800,7 @@ abstract class DbObject
 
         $this->beforeStore();
         if (array_key_exists('guid', $this->defaultProperties) && is_null($this->properties['guid'])) {
-            $this->set('guid', DbObject::generateGuid());
+            $this->set('guid', Uuid::uuid4()->toString());
         }
         $table = $this->table;
         $id = $this->getId();
@@ -1044,18 +1045,6 @@ abstract class DbObject
 
     protected function onClone()
     {
-    }
-
-    public static function generateGuid() {
-
-        if (function_exists('com_create_guid') === true) {
-            return trim(com_create_guid(), '{}');
-        }
-
-        $data    = openssl_random_pseudo_bytes(16);
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
     /**
